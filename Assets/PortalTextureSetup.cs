f10983d1ac9e6713730f478d761a71d47917cccc
NterpriseCEO <nuzumalex@gmail.com>
2022-12-02 16:00:39 +0000

    Created a basic looking house with a fire and a working tv

---
+++ Assets/PortalTextureSetup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+	// Adapted from the Brackeys portal tutorial: https://www.youtube.com/watch?v=cuQao3hEKfs
+
+	public Camera cameraA;
+	public Camera cameraB;
+
+	public Material cameraMatA;
+	public Material cameraMatB;
+
+	// Start is called before the first frame update
+	void Start() {
+		if (cameraA.targetTexture != null) {
+			cameraA.targetTexture.Release();
+		}
+		cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		cameraMatA.mainTexture = cameraA.targetTexture;
+
+		if (cameraB.targetTexture != null) {
+			cameraB.targetTexture.Release();
+		}
+		cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		cameraMatB.mainTexture = cameraB.targetTexture;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
